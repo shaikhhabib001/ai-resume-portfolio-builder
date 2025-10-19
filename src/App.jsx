b65304e9793cb6bc1ed49c12a3beb8ea107bb5b0
shaikhhabib001 <shaikhhabib619@gmail.com>
2025-10-20 04:37:37 +0500

    Vite + React + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex h-screen items-center justify-center bg-gray-800 text-white text-2xl">
+        AI Resume Builder — Ready for Setup
+      </div>
+
+    </>
+  )
+}
+
+export default App
