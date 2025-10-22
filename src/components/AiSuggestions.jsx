92913a0c8ac66efad6f38c927865cfa57c2ebda9
shaikhhabib001 <shaikhhabib619@gmail.com>
2025-10-22 15:54:47 +0500

    Updates...

--- src/components/AiSuggestions.jsx
+++ src/components/AiSuggestions.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
-const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
+const API_KEY = import.meta.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(API_KEY);
 
 const AiSuggestions = () => {
